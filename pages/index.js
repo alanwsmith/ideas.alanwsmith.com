f70d2c5443bbf6820704d76ce25d986d4bca9d7e
Alan W. Smith <alan@alanwsmith.com>
2022-01-06 00:17:38 -0500

    made open graph image a full url

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,10 @@ export default function Home() {
         />
         <meta property="og:url" content={`https://ideas.alanwsmith.com`} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta property="og:image" content="/open-graph.png" />
+        <meta
+          property="og:image"
+          content="https://ideas.alanwsmith.com/open-graph.png"
+        />
       </Head>
       <main>
         <Ideas />
