bf56e434b03cf08a5b3b5c7adf44751397a38421
Alan W. Smith <alan@alanwsmith.com>
2022-01-06 00:12:03 -0500

    Added open graph image

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,21 @@ export default function Home() {
     <div>
       <Head>
         <title>Ideas For Programming Projects</title>
+        <meta charSet="utf-8" />
         <meta
           name="description"
           content="A colleciton of ideas for programming projects."
         />
+        <meta property="og:title" content="TheIdeasOfAlan" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:creator" content="@theidofalan" />
+        <meta
+          property="og:description"
+          content="A collection of ideas for programming projects"
+        />
+        <meta property="og:url" content={`https://ideas.alanwsmith.com`} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:image" content="/open-graph.png" />
       </Head>
       <main>
         <Ideas />
