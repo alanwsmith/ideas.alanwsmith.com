b93388d59e4ba77be01fc8402c5cbf08b48ffa57
Alan Smith <commit@alanwsmith.com>
2025-01-12 01:17:59 -0500

    Pointing to new file location and added warning

--- content_builder/build_content.py
+++ content_builder/build_content.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-
 counter = 0
 with open('../components/IdeasData.js', 'w') as _output:
     _output.write(f'export const ideasData = [')
     _output.write("\n")
-    with open('/Users/alans/Dropbox/grimoire/site- Ideas For Programming Projects For The Subdomain.txt') as _input:
-        data = _input.read().split('```')[1]
+    with open('/Users/alan/Grimoire/site- Ideas For Programming Projects For The Subdomain.neo') as _input:
+        _output.write(f"// Don't edit this file directly. Update content in the grimoire. See README\n")
+        data = _input.read().split('"""')[1]
         lines = data.split("\n")
         for line in lines:
             string_list = line.split('- ')
